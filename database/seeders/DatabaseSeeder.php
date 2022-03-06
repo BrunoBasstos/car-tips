7d4856939eb026c7603a0ee5e4000d3347fc1e77
Cody McCodePants <cody@mccodepants.com>
2022-03-06 13:01:49 -0300

    feat: register the UserSeeder within DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         $this->call([
+            UserSeeder::class,
             TypeSeeder::class,
             MakeSeeder::class,
             ModelSeeder::class,
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             VehicleSeeder::class
         ]);
-
     }
 
 }
