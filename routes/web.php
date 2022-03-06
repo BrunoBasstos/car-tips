34f61de2e7eadb79b9e3363a863d2eb8f5a9055b
Cody McCodePants <cody@mccodepants.com>
2022-03-06 08:55:45 -0300

    feat: creates tips.store route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
+use App\Http\Controllers\TipController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +11,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::post('tips/store', [TipController::class, 'store'])->name('tips.store');
+
+require __DIR__ . '/auth.php';
