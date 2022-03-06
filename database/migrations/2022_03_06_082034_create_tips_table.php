0b93b80579b917a09b3bea6493c29fe8cc324d17
Cody McCodePants <cody@mccodepants.com>
2022-03-06 08:54:21 -0300

    feat: creates Tip model with resource controller, migration, factory and seeder

---
+++ database/migrations/2022_03_06_082034_create_tips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Tag;
+use App\Models\User;
+use App\Models\Vehicle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tips', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Vehicle::class)->constrained();
+            $table->foreignIdFor(Tag::class)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tips');
+    }
+};
