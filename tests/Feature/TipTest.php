d04517bff5b2b6de6931ddcea6b5e0b4f79a021a
Cody McCodePants <cody@mccodepants.com>
2022-03-06 08:04:50 -0300

    test: tip store test

--- tests/Feature/TipTest.php
+++ tests/Feature/TipTest.php
@@ -2,21 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tag;
+use App\Models\User;
+use App\Models\Vehicle;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class TipTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * @test
+     */
+    public function users_can_store_tips()
+    {
+        $user = User::factory()->createOne();
+
+        $vehicle = Vehicle::factory()->createOne();
+
+        $tag = Tag::factory()->createOne();
+
+        $data = [
+            'content' => 'Testando inserção de dicas',
+            'vehicle_id' => $vehicle->id,
+            'tag' => $tag->id
+        ];
+
+        $this->actingAs($user);
+
+        $this->post(route('tips.store'), $data)
+            ->assertStatus(Response::HTTP_CREATED);
+
+        $this->assertDatabaseHas('tips', $data);
     }
 }
