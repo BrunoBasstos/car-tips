7188ecc3d0825ebf700ff91d8e21f0ef3391a795
Cody McCodePants <cody@mccodepants.com>
2022-03-07 08:57:04 -0300

    feat: guest and authenticated users views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,37 +15,37 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    @livewireStyles
 </head>
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100">
     <!-- Page Heading -->
-    <header class="sticky top-0 z-50 bg-white shadow">
-        <div class="max-w-7xl mx-auto py-1 px-4 sm:px-6 lg:px-8">
-            <div class="flex justify-between items-center">
-                <!-- Logo -->
-                <div class="fixed top-0 left-0 px-6 py-4 sm:block">
-                    <a href="/">
-                        <x-application-logo class="block w-auto fill-current text-gray-600"/>
-                    </a>
-                </div>
-
-                @if (Route::has('login'))
-                    <div class="fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/dashboard') }}"
-                               class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}"
-                               class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Login') }}</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}"
-                                   class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Register') }}</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
+    <header class="sticky top-0 bg-white shadow h-24">
+        <div class="flex justify-between align-middle">
+            <!-- Logo -->
+            <div class="fixed top-0 left-0 px-6 py-4 sm:block">
+                <a href="/">
+                    <x-application-logo class="block w-auto fill-current text-gray-600"/>
+                </a>
             </div>
+
+            @if (Route::has('login'))
+                <div class="fixed top-0 right-0 px-6 p-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}"
+                           class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}"
+                           class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Login') }}</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                               class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Register') }}</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
         </div>
     </header>
 
@@ -54,13 +54,13 @@
         <div class="py-12">
             <div class="mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <x-grid.grid
-                        title="Dicas"
-                        />
+                    <livewire:tips/>
                 </div>
             </div>
         </div>
     </main>
+
+    @livewireScripts
 </div>
 </body>
 </html>
