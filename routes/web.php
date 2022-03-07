1d18e2cd9e2c579fb7897c5211ee2aac5eec3f13
Cody McCodePants <cody@mccodepants.com>
2022-03-07 08:53:49 -0300

    feat: protectes dashboard and tips routes and keep root route for non-authenticated users

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TipController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', HomeController::class);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+    Route::resource('tips', TipController::class);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::post('tips/store', [TipController::class, 'store'])->name('tips.store');
 
 require __DIR__ . '/auth.php';
