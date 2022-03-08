be1c5938d67c79581aced3ac599b73faf8b4164d
Cody McCodePants <cody@mccodepants.com>
2022-03-08 14:47:30 -0300

    refactor: renamming component tips.grid

--- resources/views/components/tips-grid/main.blade.php
+++ resources/views/components/tips-grid/main.blade.php
@@ -1,11 +1,11 @@
 <div class="p-6 border-t border-gray-200 dark:border-gray-700 w-full">
     <div class="sm:px-6 w-full">
-        <x-grid.title :title="$title"/>
+        <x-tips-grid.title :title="$title"/>
 
         <div class="bg-white py-4 md:py-7 px-4 md:px-8 xl:px-10">
-            <x-grid.filter/>
+            <x-tips-grid.filter/>
 
-            <x-grid.table/>
+            <x-tips-grid.table/>
         </div>
     </div>
 </div>
